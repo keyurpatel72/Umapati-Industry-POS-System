b79756b994f9f345ee715f48fa2373099c59ad17
keyur patel <63536896+keyurpatel72@users.noreply.github.com>
2024-08-20 16:36:17 +0530

    Add files via upload

---
+++ Umapati-Industry-POS-System/utils/data.js
@@ -0,0 +1,43 @@
+const items = [
+  {
+    name: "pen",
+    price: 1,
+    category: "dosa Tawa",
+    image:
+    "https://mycoconutworld.com/cdn/shop/products/2466-e1643884438514_2048x2048.png?v=1673512159",
+  },
+  {
+    name: "ciba",
+    price: 2,
+    category: "set",
+    image: "https://mycoconutworld.com/cdn/shop/products/189-2-e1657101582685_1024x1024.jpg?v=1673512450",
+  },
+  {
+    name: "Chicken Biryani",
+    price: 10,
+    category: "rice",
+    image:
+      "https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg",
+  },
+  {
+    name: " Spoon",
+    price: 5,
+    category: "Baby Spoon",
+    image:
+      "https://mycoconutworld.com/collections/kitchen-tools/products/coconut-sancy-stainless-steel-6-baby-spoon-6-mater-spoon-6-fork-6-knife-with-wooden-stand-1-unit",
+  },
+  {
+    name: "Serving Spoon",
+    price: 7,
+    category: "Set Serving Spoon",
+    image: "https://mycoconutworld.com/cdn/shop/products/2-39_1024x1024.jpg?v=1673515908",
+  },
+  {
+    name: "Breakfast Plate/Snack Plate/Desert Plate ",
+    price: 5,
+    category: "plate",
+    image:
+      "https://mycoconutworld.com/cdn/shop/files/H20OrangePlate_3_2048x2048.jpg?v=1710751933",
+  },
+];
+module.exports = items;
